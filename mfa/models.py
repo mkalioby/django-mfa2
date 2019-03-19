82fd6f28bde70a608ef9c59e77f6a0de05d38f52
Mohamed ElKalioby <melkalioby@kfshrc.edu.sa>
2019-03-19 12:58:58 +0300

    Fixing model name

--- mfa/models.py
+++ mfa/models.py
@@ -17,6 +17,9 @@ class User_Keys(models.Model):
         if self.key_type == "Trusted Device" and self.properties.get("signature","") == "":
             self.properties["signature"]= jwt.encode({"username": self.username, "key": self.properties["key"]}, settings.SECRET_KEY)
         super(User_Keys, self).save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
-
+    def __unicode__(self):
+        return "%s -- %s"%(self.username,self.key_type)
+    def __str__(self):
+        return self.__unicode__()
     class Meta:
         app_label='mfa'
