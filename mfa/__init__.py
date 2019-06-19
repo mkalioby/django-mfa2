087ea69de15d59d56577434b1cf1d8cac3cc48eb
Mohamed ElKalioby <melkalioby@kfshrc.edu.sa>
2019-06-19 14:04:57 +0300

    Adding __version__

--- mfa/__init__.py
+++ mfa/__init__.py
@@ -1,10 +1,1 @@
-# @property
-# def urls():
-#      import django
-#      if django.VERSION < (1, 9):
-#          from .mfa_urls import url_patterns
-#          return url_patterns, 'mfa', ''
-#      else:
-#          from .mfa_urls import url_patterns
-#          return url_patterns,'mfa'
-#
+__version__="1.4.1"
