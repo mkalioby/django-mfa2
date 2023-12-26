6ae4c2508c2faae5ab29c9c459d3173739dd910a
Oussama Jarrousse <oussama@jarrousse.org>
2023-12-26 17:33:21 +0100

    2 passed in 0.82s, 32% coverage

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,2 +1,25 @@
 import pytest
+from django.urls import reverse
+
+@pytest.mark.django_db
+def test_index_unauthenticated(client):
+    url = reverse("mfa_home")
+    response = client.get(url)
+    assert response is not None
+    assert response.status_code == 302
+    assert response.url=="/accounts/login/?next=/"
+
+
+@pytest.mark.django_db
+def test_index_authenticated(client, authenticated_user):
+    url = reverse("mfa_home")
+    response = client.get(url)
+    assert response is not None
+    assert response.status_code == 200
+    assert isinstance(response.templates, list)
+    assert len(response.templates) == 4
+    for template in response.templates:
+        assert template.name in ["modal.html", "base.html", "mfa_base.html", "MFA.html"]
+
+
 
