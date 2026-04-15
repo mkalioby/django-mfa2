319d14f77d2b504f874f8b850555a1756376f451
Mohamed ElKalioby <mkalioby@mkalioby.com>
2026-04-15 12:00:12 +0300

    Bumped version of FIDO2 till 2.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-mfa2",
-    version="3.1.1",
+    version="3.2b2",
     description="Allows user to add 2FA to their accounts",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -21,7 +21,7 @@ setup(
         "ua-parser",
         "user-agents",
         "python-jose",
-        "fido2 >= 1.1.1,<2.0",
+        "fido2 >= 1.1.1,<2.3",
     ],
     python_requires=">=3.5",
     include_package_data=True,
